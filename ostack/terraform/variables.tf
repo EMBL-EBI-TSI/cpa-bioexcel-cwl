ab5ea5443fc9737b44b3e9ab694ed980c14ddb6e
Ania Niewielska <aniewielska@ebi.ac.uk>
2021-08-12 14:57:23 +0100

    Update variables.tf

--- ostack/terraform/variables.tf
+++ ostack/terraform/variables.tf
@@ -36,29 +36,29 @@ variable "network_name" {
 #### SSH ####
 variable "public_key_path" {
   description = "The path of the public SSH key to be injected in the vm"
-  type        = "string"
+  type        = string
   default     = "~/.ssh/id_rsa.pub"
 }
 
 variable "private_key_path" {
   description = "The path of the private SSH key to be used to connect to the the vm"
-  type        = "string"
+  type        = string
   default     = "~/.ssh/id_rsa"
 }
 
 variable "ssh_key" {
   description = "The public SSH key to be injected in the vm"
-  type        = "string"
+  type        = string
 }
 
 variable "ssh_user" {
   default     = "ubuntu"
   description = "SSH user to login to VM"
-  type        = "string"
+  type        = string
 }
 
 variable "user_private_key_path" {
   description = "The path of the private SSH key connected to the public SSH key to be injected in the vm. This key will be not used by terraform or ansible, but the path will used only to build a personalised ssh.config file. If not set up, it will assume the key is located in ~/.ssh/id_rsa"
-  type        = "string"
+  type        = string
   default     = "~/.ssh/id_rsa"
 }
