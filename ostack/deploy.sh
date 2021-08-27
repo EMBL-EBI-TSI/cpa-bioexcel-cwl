c19d03b5e4d05ec043f0ae9b488da02914d17a7a
gef <gef@ebi.ac.uk>
2021-08-27 22:04:02 +0100

    Updated deploy.sh (copied across from gef-ebi/cpa-instance)

--- ostack/deploy.sh
+++ ostack/deploy.sh
@@ -29,7 +29,7 @@ echo "export KEY_PATH=${KEY_PATH}"
 export TF_VAR_key_path="${KEY_PATH}"
 echo "export TF_VAR_key_path=${TF_VAR_key_path}"
 
-export TF_STATE=${DPL}
+export TF_STATE=${DPL}'terraform.tfstate'
 echo "export TF_STATE=${TF_STATE}"
 
 # Launch provisioning of the VM
@@ -45,6 +45,13 @@ echo -e "\n\t${CYAN}Install Ansible requirements with ansible galaxy${NC}\n"
 cd ostack/ansible || exit
 ansible-galaxy install -r requirements.yml
 
+# Used by terraform.py - env var holds directory containing terraform.tfstate file
+export TERRAFORM_STATE_ROOT=${DPL}
+echo "export TERRAFORM_STATE_ROOT=${DPL}"
+# Used by ansible-playbook - local var holds host name (e.g. tsi1629995083720-1)
+terraform_default_host="${TF_VAR_name}-1"
+echo "Script var terraform_default_host=\"${TF_VAR_name}-1\""
+
 # Set default value for Ansible variables if they are either empty or undefined
 export TF_VAR_remote_folder="${TF_VAR_remote_folder:-/var/nfs}"
 echo "export TF_VAR_remote_folder=${TF_VAR_remote_folder}"
@@ -55,4 +62,4 @@ echo "export ANSIBLE_REMOTE_USER=${ANSIBLE_REMOTE_USER}"
 
 # Launch Ansible playbook
 echo -e "\n\t${CYAN}Launch Ansible playbook${NC}\n"
-ansible-playbook -b playbook.yml
+ansible-playbook -vvvv -b playbook.yml --extra-vars "ecp_host=${terraform_default_host}"
