be122c65d3f59b15c082dcf52bf580a566b4372e
gef <gef@ebi.ac.uk>
2021-08-27 23:13:23 +0100

    main.tf changes relocated to provider.tf

--- ostack/terraform/provider.tf
+++ ostack/terraform/provider.tf
@@ -7,3 +7,10 @@ terraform {
      }
    }
  }
+
+provider "openstack" {
+  user_name = "${var.user_name}"
+  tenant_name = "${var.tenant_name}"
+  auth_url = "${var.auth_url}"
+  region = "${var.region}"
+}
