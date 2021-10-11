db994165a488769bcc1363dfa5bff0b1b4024d1c
gef <gef@ebi.ac.uk>
2021-10-11 17:59:52 +0100

    Adjust state to json output (2)

--- ostack/state.sh
+++ ostack/state.sh
@@ -5,4 +5,4 @@ set -e
 # Query Terraform state file
 export APP="${PORTAL_APP_REPO_FOLDER}"
 export DPL="${PORTAL_DEPLOYMENTS_ROOT}/${PORTAL_DEPLOYMENT_REFERENCE}/"
-terraform -chdir=${APP} show -json ${DPL}'terraform.tfstate'
+terraform -chdir=${APP}'/ostack/terraform'  show -json ${DPL}'terraform.tfstate'
